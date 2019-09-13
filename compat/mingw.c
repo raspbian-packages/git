007229e8e60d50ab8fbb33198e5022e08bb4a000
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-09-13 16:32:43 +0200

    mingw: fix quoting of arguments
    We need to be careful to follow proper quoting rules. For example, if an
    argument contains spaces, we have to quote them. Double-quotes need to
    be escaped. Backslashes need to be escaped, but only if they are
    followed by a double-quote character.
    
    We need to be _extra_ careful to consider the case where an argument
    ends in a backslash _and_ needs to be quoted: in this case, we append a
    double-quote character, i.e. the backslash now has to be escaped!
    
    The current code, however, fails to recognize that, and therefore can
    turn an argument that ends in a single backslash into a quoted argument
    that now ends in an escaped double-quote character. This allows
    subsequent command-line parameters to be split and part of them being
    mistaken for command-line options, e.g. through a maliciously-crafted
    submodule URL during a recursive clone.
    
    Technically, we would not need to quote _all_ arguments which end in a
    backslash _unless_ the argument needs to be quoted anyway. For example,
    `test\` would not need to be quoted, while `test \` would need to be.
    
    To keep the code simple, however, and therefore easier to reason about
    and ensure its correctness, we now _always_ quote an argument that ends
    in a backslash.
    
    This addresses CVE-2019-1350.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    (cherry picked from commit 6d8684161ee9c03bed5cb69ae76dfdddb85a0003)
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    
    Gbp-Pq: Name 0018-mingw-fix-quoting-of-arguments.diff

--- compat/mingw.c
+++ compat/mingw.c
@@ -1054,7 +1054,7 @@ static const char *quote_arg_msvc(const char *arg)
 				p++;
 				len++;
 			}
-			if (*p == '"')
+			if (*p == '"' || !*p)
 				n += count*2 + 1;
 			continue;
 		}
@@ -1076,16 +1076,19 @@ static const char *quote_arg_msvc(const char *arg)
 				count++;
 				*d++ = *arg++;
 			}
-			if (*arg == '"') {
+			if (*arg == '"' || !*arg) {
 				while (count-- > 0)
 					*d++ = '\\';
+				/* don't escape the surrounding end quote */
+				if (!*arg)
+					break;
 				*d++ = '\\';
 			}
 		}
 		*d++ = *arg++;
 	}
 	*d++ = '"';
-	*d++ = 0;
+	*d++ = '\0';
 	return q;
 }
 
