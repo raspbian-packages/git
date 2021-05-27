d7de6b9aea566957f94950603d047cdd306b0dbe
Elijah Newren <newren@gmail.com>
2021-05-27 04:53:56 +0000

    dir: introduce readdir_skip_dot_and_dotdot() helper
    Many places in the code were doing
        while ((d = readdir(dir)) != NULL) {
            if (is_dot_or_dotdot(d->d_name))
                continue;
            ...process d...
        }
    Introduce a readdir_skip_dot_and_dotdot() helper to make that a one-liner:
        while ((d = readdir_skip_dot_and_dotdot(dir)) != NULL) {
            ...process d...
        }
    
    This helper particularly simplifies checks for empty directories.
    
    Also use this helper in read_cached_dir() so that our statistics are
    consistent across platforms.  (In other words, read_cached_dir() should
    have been using is_dot_or_dotdot() and skipping such entries, but did
    not and left it to treat_path() to detect and mark such entries as
    path_none.)
    
    Signed-off-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    (cherry picked from commit 906fc557b70b2b2995785c9b37e212d2f86b469e)
    
    [ELTS] This helper function is required strictly as a dependency for
           CVE-2024-32002-3.patch, and so I have not backported all the code
           replacements from the upstream patch, only the addition of the new
           function.
    
    Gbp-Pq: Name CVE-2024-32002-2.patch

--- dir.h
+++ dir.h
@@ -338,6 +338,8 @@ struct dir_struct {
 	unsigned unmanaged_exclude_files;
 };
 
+struct dirent *readdir_skip_dot_and_dotdot(DIR *dirp);
+
 /*Count the number of slashes for string s*/
 int count_slashes(const char *s);
 
