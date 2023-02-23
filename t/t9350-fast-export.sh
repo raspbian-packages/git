314108daea3c3f759abb6abfb11dcf71c4f1b2df
Gerrit Pape <pape@smarden.org>
2023-02-23 15:14:14 +0000

    CVE-2022-39253-10
    Origin: https://github.com/git/git/commit/f4a32a550f9d40471fb42ed1e5c8612dfe4a83b1
    Reviewed-by: Sylvain Beucler <beuc@debian.org>
    Last-Update: 2022-12-12
    
    From f4a32a550f9d40471fb42ed1e5c8612dfe4a83b1 Mon Sep 17 00:00:00 2001
    From: Taylor Blau <me@ttaylorr.com>
    Date: Fri, 29 Jul 2022 15:21:53 -0400
    Subject: [PATCH] t/t9NNN: allow local submodules
    
    To prepare for the default value of `protocol.file.allow` to change to
    "user", ensure tests that rely on local submodules can initialize them
    over the file protocol.
    
    Tests that interact with submodules a handful of times use
    `test_config_global`.
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>
    
    Gbp-Pq: Name CVE-2022-39253-10.patch

--- t/t9350-fast-export.sh
+++ t/t9350-fast-export.sh
@@ -149,6 +149,7 @@ test_expect_success 'signed-tags=warn-strip' '
 
 test_expect_success 'setup submodule' '
 
+	test_config_global protocol.file.allow always &&
 	git checkout -f master &&
 	mkdir sub &&
 	(
@@ -174,6 +175,7 @@ test_expect_success 'setup submodule' '
 
 test_expect_success 'submodule fast-export | fast-import' '
 
+	test_config_global protocol.file.allow always &&
 	SUBENT1=$(git ls-tree master^ sub) &&
 	SUBENT2=$(git ls-tree master sub) &&
 	rm -rf new &&
